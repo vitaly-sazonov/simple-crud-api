0a03af643a78cd3ebfc20f48c8e0776a6f8073cb
vitomax <v.o.sazonov@gmail.com>
2021-11-26 19:31:56 +0300

    feat: add RED Test for CRUD api

--- __tests__/e2e.test.js
+++ __tests__/e2e.test.js
@@ -51,3 +51,84 @@ describe('GREEN Test CRUD', () => {
     expect(res.body.msg).toBe('The record not found');
   });
 });
+
+describe('RED Test CRUD', () => {
+  let uuid = '';
+  const body = { name: 'Vasya', age: '20', hobbies: "['walk','fight','drink']" };
+
+  test('GET /person/:bad_uuid -> return {msg:"Bad uuid"} and statusCode 400', async () => {
+    const res = await request.get(`/person/asde`).set('Accept', 'application/json');
+    expect(res.statusCode).toBe(400);
+    expect(res.body.msg).toBe('Bad uuid');
+  });
+
+  test('GET /person/:valid_id "but not record" -> return {msg:"The record not found"} and statusCode 404', async () => {
+    const res = await request
+      .get(`/person/ad064eab-0129-4dec-9be3-2e259cd65b4e`)
+      .set('Accept', 'application/json');
+    expect(res.statusCode).toBe(404);
+    expect(res.body.msg).toBe('The record not found');
+  });
+
+  test(`POST /person/ {name, age} "no required property" -> return {msg:"Fields 'name', 'age' and 'hobby' must be specified"} and statusCode 400`, async () => {
+    const res = await request
+      .post(`/person`)
+      .set('Accept', 'application/json')
+      .send({ name: 'Vasya', age: '20' });
+    expect(res.statusCode).toBe(400);
+    expect(res.body.msg).toBe(`Fields 'name', 'age' and 'hobby' must be specified`);
+  });
+
+  test('PUT /person/:bad_uuid -> return {msg:"Bad uuid"} and statusCode 400', async () => {
+    const res = await request.put(`/person/asde`).set('Accept', 'application/json');
+    expect(res.statusCode).toBe(400);
+    expect(res.body.msg).toBe('Bad uuid');
+  });
+
+  test('PUT /person/:valid_id "but not record" -> return {msg:"The record not found"} and statusCode 404', async () => {
+    const res = await request
+      .put(`/person/ad064eab-0129-4dec-9be3-2e259cd65b4e`)
+      .set('Accept', 'application/json')
+      .send(body);
+    expect(res.statusCode).toBe(404);
+    expect(res.body.msg).toBe('The record not found');
+  });
+
+  test('DELETE /person/:bad_uuid -> return {msg:"Bad uuid"} and statusCode 400', async () => {
+    const res = await request.delete(`/person/asde`).set('Accept', 'application/json');
+    expect(res.statusCode).toBe(400);
+    expect(res.body.msg).toBe('Bad uuid');
+  });
+
+  test('DELETE /person/:valid_id "but not record" -> return {msg:"The record not found"} and statusCode 404', async () => {
+    const res = await request
+      .delete(`/person/ad064eab-0129-4dec-9be3-2e259cd65b4e`)
+      .set('Accept', 'application/json');
+    expect(res.statusCode).toBe(404);
+    expect(res.body.msg).toBe('The record not found');
+  });
+
+  test(`POST /person/ {incorrect json} -> return {msg:"Internal server error"} and statusCode 500`, async () => {
+    const res = await request
+      .post(`/person`)
+      .set('Accept', 'application/json')
+      .send(`{ name:Vasya age: '20' }`);
+    expect(res.statusCode).toBe(500);
+    expect(res.body.msg).toBe(`Internal server error`);
+  });
+
+  test(`POST /person/ {incorrect json} -> return {msg:"Internal server error"} and statusCode 500`, async () => {
+    const res = await request
+      .post(`/person`)
+      .set('Accept', 'application/json')
+      .send(`{ name:Vasya age: '20' }`);
+    expect(res.statusCode).toBe(500);
+    expect(res.body.msg).toBe(`Internal server error`);
+  });
+
+  test(`GET /person/path/to/param/ -> return {msg:"Not Found"} and statusCode 404`, async () => {
+    const res = await request.get(`/person/path/to/param/`).set('Accept', 'application/json');
+    expect(res.statusCode).toBe(404);
+    expect(res.body.msg).toBe(`Not Found`);
+  });
+});
